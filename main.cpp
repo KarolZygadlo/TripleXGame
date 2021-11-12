f70f1943a4f0f0c7aaf91f215acaf8c6cd3b9cc1
Karol Zygadło <karol.zygadlo9713@gmail.com>
2021-11-12 14:29:04 +0100

    update, completed the simple text and console based game with C++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,69 @@
 #include <iostream>
+#include <ctime>
+
+void PrintIntroduction(int Difficulty)
+{
+    // Print welcome messages to the terminal
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << "secure server room...\nYou need to enter the correct codes to continue...\n";
+}
+
+bool PlayGame(int Difficulty)
+{
+    PrintIntroduction(Difficulty);
+
+    // Declare 3 number code
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    // Print sum and product to the terminal
+    std::cout << "+ There are 3 numbers in the code\n"; 
+    std::cout << "+ The codes add-up to: " << CodeSum; 
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
+
+    // Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    const int GuessSum = GuessA + GuessB + GuessC;
+    const int GuessProduct = GuessA * GuessB * GuessC;
+
+    std::cout << "\n+ You entered: " << GuessA << GuessB << GuessC;
+
+    // Check if the player guess is correct
+    if(GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "\n*** Well done agent! You have extracted a file! Keep going! ***";
+        return true; 
+    }
+    else 
+    {
+        std::cout << "\n*** You entered the wrong code! Careful agent! Try again! ***";
+        return false;
+    }
+}
 
 int main()
 {
-    std::cout << "hello world\n";
+    srand(time(NULL));
+
+    int LevelDifficulty = 1;
+    const int MaxDifficulty = 20;
+    while (LevelDifficulty <= MaxDifficulty)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+    }
+    std::cout << "\n*** Great work agent! You got all the files! Now get out of there! ***\n";
     return 0;
 }
